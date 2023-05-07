a3940101b5b464b492358be999c1726af7ba8b6f
pfigiel <01178639@pw.edu.pl>
2023-05-07 03:43:41 +0200

    Stworzenie gita, dodanie .gitignore i napisanie wzorcu pomijanych plików, utworzenie głównej klasy i okna

---
+++ src/FrameMain.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class FrameMain extends JFrame {
+
+    FrameMain(){
+        this.setLayout(null);
+        this.setSize(new Dimension(700, 700));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
---
+++ src/KakuroMain.java
@@ -0,0 +1,8 @@
+public class KakuroMain {
+    public static void main(String[] args) {
+
+        // nasza główna klasa, w której będzie inicjowane okno programu
+        
+        FrameMain frameMain = new FrameMain();
+    }
+}
